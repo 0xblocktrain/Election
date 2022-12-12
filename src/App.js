c61596d73998a323771b59c36857ea43e3a37340
Deveshb15 <deveshb583@gmail.com>
2022-12-12 21:21:07 +0530

    chore: rainbowkit installed and configured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-6xl font-extrabold">Election DApp</h1>
-    </div>
-  );
+	return (
+		<div className="bg-black text-white">
+			<div className="flex items-center justify-between flex-row px-4 py-2">
+				{/* Logo */}
+				<h1 className="text-2xl font-bold">Election</h1>
+				<ConnectButton />
+			</div>
+			<div className="flex items-center justify-center min-h-screen">
+				<h1 className="text-4xl font-extrabold">Vote</h1>
+			</div>
+		</div>
+	);
 }
 
 export default App;
